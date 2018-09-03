b8adf6586c9bde72448fed35ad774c9eb8fdae4b
Marcus Nunes <marcus.nunes@gmail.com>
2018-09-03 15:12:38 -0300

    Link em nova janela
    Agora os link abrem em uma nova janela no navegador

--- ui.R
+++ ui.R
@@ -67,10 +67,10 @@ dashboardPage(
                   tabPanel("Desenvolvedores", h2("Marcus Alexandre Nunes"), align = "left", " Doutor em Estatística pela Penn State University", br(),
                            "Mestre em Matemática Pura pela Universidade Federal do Rio Grande do Sul", br(),
                            "Bacharel em Matemática Aplicada e Computacional pela Universidade Federal do Rio Grande do Sul",br(),
-                           a("https://marcusnunes.me", href="https://marcusnunes.me"),br(),
-                           a("https://github.com/mnunes", href="https://github.com/mnunes"),
+                           a("https://marcusnunes.me", href="https://marcusnunes.me", target="_blank"),br(),
+                           a("https://github.com/mnunes", href="https://github.com/mnunes", target="_blank"),
                            br(),
-                           h2("Rayland Matos Magalhães"),"Graduando em Estatística pela Universidade Federal do Rio Grande do Norte",br(),a("https://github.com/raylandmagalhaes", href="https://github.com/raylandmagalhaes")),
+                           h2("Rayland Matos Magalhães"),"Graduando em Estatística pela Universidade Federal do Rio Grande do Norte",br(),a("https://github.com/raylandmagalhaes", href="https://github.com/raylandmagalhaes", target="_blank")),
                   
                   tabPanel("Projeto","Esta plataforma é um projeto de iniciação científica que tem como orientador o professor", strong("MARCUS ALEXANDRE NUNES"), "e como discente voluntário, o aluno de graduação", strong("RAYLAND MATOS MAGALHÃES")," (vide aba 'Desenvolvedores') ",br(),br(),
                            p("A Lei de Acesso à informação, promulgada em 16 de maio de 2012, trouxe uma quantidade de informação sem precedentes para a sociedade brasileira. Através dela, os órgãos públicos integrantes da administração direta dos Poderes Executivo, Legislativo, incluindo as Cortes de Contas, e Judiciário e do Ministério Público devem informar seus gastos com material, salários, licitações e tudo o mais que envolver dinheiro público."),
