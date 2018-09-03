e7d0773792a6cceb5532ad85eefe27c19db9e093
Marcus Nunes <marcus.nunes@gmail.com>
2018-09-02 21:17:29 -0300

    Merge desfeito

--- global.R
+++ global.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 library(reembolsos)
 library(tidyverse)
 library(ggplot2)
@@ -6,14 +5,3 @@ library(shinydashboard)
 library(treemap)
 library(gridExtra)
 library(scales)
-=======
-library(reembolsos)
-library(tidyverse)
-library(ggplot2)
-library(shinydashboard)
-library(treemap)
-library(gridExtra)
-library(scales)
-library(plotly)
-#load("camara")
->>>>>>> cdb3b66bd7c0f2a57d26286de4e7e36dbb40856b
