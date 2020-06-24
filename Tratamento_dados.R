a9cc96b35bc76d23531bf072d0862e3ac15a5ba7
Marcus Nunes <marcus.nunes@gmail.com>
2020-06-24 09:08:47 -0300

    Adição dos dados de 2020

--- Tratamento_dados.R
+++ Tratamento_dados.R
@@ -1,7 +1,7 @@
 library(reembolsos)
 library(tidyverse)
 
-camara=bind_rows(list(camara2009,camara2010,camara2011,camara2012,camara2013,camara2014,camara2015,camara2016,camara2017,camara2018,camara2019)) %>%
+camara=bind_rows(list(camara2009,camara2010,camara2011,camara2012,camara2013,camara2014,camara2015,camara2016,camara2017,camara2018,camara2019,camara2020)) %>%
   filter(total_net_value>0) %>%
   data.frame(check.names = F) %>% 
   filter(!is.na(total_net_value)&!is.na(document_value)) %>%
